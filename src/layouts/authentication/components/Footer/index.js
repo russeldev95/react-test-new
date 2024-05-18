e1264c9e2f60adf32ea0e25b3eb22e6a20d8c049
russeldev95 <163489349+russeldev95@users.noreply.github.com>
2024-05-18 16:00:53 +0800

    first and final commit

---
+++ src/layouts/authentication/components/Footer/index.js
@@ -0,0 +1,69 @@
+/** 
+
+=========================================================
+* Vision UI PRO React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-dashboard-pro-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Visionware.
+
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+// Vision UI Dashboard PRO React components
+import VuiBox from "components/VuiBox";
+import VuiTypography from "components/VuiTypography";
+
+function Footer({ full }) {
+  return (
+    <VuiBox
+      component="footer"
+      py={6}
+      sx={({ breakpoints }) => ({
+        maxWidth: full ? "100%" : "450px",
+        [breakpoints.down("xl")]: {
+          maxWidth: full ? "100%" : "400px",
+        },
+      })}
+    >
+      <Grid
+        container
+        justifyContent={{ xs: "center", lg: full ? "space-between" : "center" }}
+        sx={({ breakpoints }) => ({
+          maxWidth: full ? "100%" : "450px",
+          [breakpoints.down("xl")]: {
+            maxWidth: full ? "100%" : "400px",
+          },
+        })}
+      >
+        <Grid item xs={full ? 12 : 12} lg={full ? 6 : 12} sx={{ textAlign: "center" }}>
+          <VuiTypography
+            variant="button"
+            sx={{ textAlign: "center", fontWeight: "400 !important" }}
+            color="white"
+          >
+            Powered by together, a visionary product of Fireworks Digital
+© 2024 Mobility Asia Sdn. Bhd. (Registration No: 1257623-M)
+          </VuiTypography>
+        </Grid>
+      </Grid>
+    </VuiBox>
+  );
+}
+
+export default Footer;
---
+++ src/layouts/authentication/sign-up/cover/index.js
@@ -0,0 +1,187 @@
+/** 
+
+=========================================================
+* Vision UI PRO React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-dashboard-pro-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Visionware.
+
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Icon from "@mui/material/Icon";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+
+// Icons
+import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
+
+// Vision UI Dashboard React components
+import VuiBox from "components/VuiBox";
+import VuiTypography from "components/VuiTypography";
+import VuiInput from "components/VuiInput";
+import VuiButton from "components/VuiButton";
+import VuiSwitch from "components/VuiSwitch";
+import GradientBorder from "examples/GradientBorder";
+
+// Vision UI Dashboard assets
+import radialGradient from "assets/theme/functions/radialGradient";
+import rgba from "assets/theme/functions/rgba";
+import palette from "assets/theme/base/colors";
+import borders from "assets/theme/base/borders";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import bgImage from "assets/images/background-cover-auth-signup.png";
+
+function SignIn() {
+  const [rememberMe, setRememberMe] = useState(true);
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  return (
+    <CoverLayout
+      title="together"
+      color="white"
+      description="COMMUNITY POWERED COMMERCE"
+      image={bgImage}
+      premotto={"COMMUNITY POWERED COMMERCE"}
+      motto={"together"}
+      cardContent
+    >
+      <GradientBorder borderRadius={borders.borderRadius.form} minWidth="100%" maxWidth="100%">
+        <VuiBox
+          component="form"
+          role="form"
+          borderRadius="inherit"
+          p="45px"
+          sx={({ palette: { secondary } }) => ({
+            backgroundColor: secondary.focus,
+          })}
+        >
+          <VuiBox mb={2}>
+            <VuiBox mb={1} ml={0.5}>
+              <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
+                Name
+              </VuiTypography>
+            </VuiBox>
+            <GradientBorder
+              minWidth="100%"
+              borderRadius={borders.borderRadius.lg}
+              padding="1px"
+              backgroundImage={radialGradient(
+                palette.gradients.borderLight.main,
+                palette.gradients.borderLight.state,
+                palette.gradients.borderLight.angle
+              )}
+            >
+              <VuiInput
+                placeholder="Your name..."
+                sx={({ typography: { size } }) => ({
+                  fontSize: size.sm,
+                })}
+              />
+            </GradientBorder>
+          </VuiBox>
+          <VuiBox mb={2}>
+            <VuiBox mb={1} ml={0.5}>
+              <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
+                Email
+              </VuiTypography>
+            </VuiBox>
+            <GradientBorder
+              minWidth="100%"
+              borderRadius={borders.borderRadius.lg}
+              padding="1px"
+              backgroundImage={radialGradient(
+                palette.gradients.borderLight.main,
+                palette.gradients.borderLight.state,
+                palette.gradients.borderLight.angle
+              )}
+            >
+              <VuiInput
+                type="email"
+                placeholder="Your email..."
+                sx={({ typography: { size } }) => ({
+                  fontSize: size.sm,
+                })}
+              />
+            </GradientBorder>
+          </VuiBox>
+          <VuiBox mb={2}>
+            <VuiBox mb={1} ml={0.5}>
+              <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
+                Password
+              </VuiTypography>
+            </VuiBox>
+            <GradientBorder
+              minWidth="100%"
+              borderRadius={borders.borderRadius.lg}
+              padding="1px"
+              backgroundImage={radialGradient(
+                palette.gradients.borderLight.main,
+                palette.gradients.borderLight.state,
+                palette.gradients.borderLight.angle
+              )}
+            >
+              <VuiInput
+                type="password"
+                placeholder="Your password..."
+                sx={({ typography: { size } }) => ({
+                  fontSize: size.sm,
+                })}
+              />
+            </GradientBorder>
+          </VuiBox>
+          <VuiBox display="flex" alignItems="center">
+            <VuiSwitch color="info" checked={rememberMe} onChange={handleSetRememberMe} />
+            <VuiTypography
+              variant="caption"
+              color="white"
+              fontWeight="medium"
+              onClick={handleSetRememberMe}
+              sx={{ cursor: "pointer", userSelect: "none" }}
+            >
+              &nbsp;&nbsp;&nbsp;&nbsp;Remember me
+            </VuiTypography>
+          </VuiBox>
+          <VuiBox mt={4} mb={1}>
+            <VuiButton color="info" fullWidth>
+              SIGN UP
+            </VuiButton>
+          </VuiBox>
+          <VuiBox mt={3} textAlign="center">
+            <VuiTypography variant="button" color="text" fontWeight="regular">
+              Already have an account?{" "}
+              <VuiTypography
+                component={Link}
+                to="/authentication/sign-in/cover"
+                variant="button"
+                color="white"
+                fontWeight="medium"
+              >
+                Sign in
+              </VuiTypography>
+            </VuiTypography>
+          </VuiBox>
+        </VuiBox>
+      </GradientBorder>
+    </CoverLayout>
+  );
+}
+
+export default SignIn;
